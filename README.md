11e04eeec48c51a0b5509641dad437793f7ef6f1
Nick Pascucci <npascut1@gmail.com>
2020-01-17 20:41:44 +0000

    Build logic to extract code fragments based on comment delimiters.

---
+++ README.md
@@ -0,0 +1,99 @@
+# Verso / Recto - A Different Approach to Literate Programming
+
+> Literate programming is the art of preparing programs for human readers.
+
+_Norman Ramsey, on the [noweb homepage](https://www.cs.tufts.edu/~nr/noweb/)._
+
+Literate Programming (LP) tries to address a common problem with software systems: by reading the
+code one can discover _how_ a thing is done, but not _why_ it is done that way. Every program has a
+theory of operation embedded within its logic, but this is often hidden. Comments within the code
+and documentation of APIs are helpful but insufficient for this task. Often the documentation
+generation systems provided with a programming language do not provide a way to contextualize the
+code they describe within the larger system. For programs which rely on a more advanced mathematical
+background, it is also often difficult to embed equations or other markup in the documentation.
+
+Existing LP tools such as WEB, noweb, and Org Babel attempt to address this by taking the following
+approach:
+
+1. Embed the program source code within a prose document which explains the author's thinking.
+2. Provide mechanisms for abstracting code into abstract chunks, which can be recombined in
+   different orders than they are defined and referenced throughought the document.
+3. Process the combined document in one of two ways: either `tangle` the source code out of the
+   document into a computer-friendly version, or `weave` the document into a form ready for humans
+   to read.
+
+Overall this is an improvement on inline documentation and can provide much more context to the
+reader than mainstream approaches:
+
+- The source code is embedded within a document, making it inaccessible to language-specific tooling
+  such as editors, compilers, and static analysis tools. In order to use these the code must first
+  be tangled.
+- Similarly, to build your literate program the end user must have the appropriate tools installed
+  in addition to the language tooling needed to compile the source.
+- Most programmers have spent years working directly in the machine-friendly source representation
+  rather than a literate style, introducing a barrier to entry to writing literate programs. Porting
+  an existing project to a literate representation is nontrivial.
+
+`Verso / Recto` takes a different approach. It considers the source file as a first-class citizen to
+be referenced by the documentation. Rather than embedding source code in documents, or documents in
+source code, lightweight annotations are used to mark sections of interest within the code which can
+be easily referenced by prose documents. There is no `tangle` step, and source files remain fully
+valid inputs for compilers, editors, and other tools. There is no need to maintain line numbers or
+formatting between literate sources and what the compiler sees.
+
+## What traditional LP systems get right
+
+A lot of things! Here are a few ideas that the traditional systems pioneered and `Verso / Recto`
+borrows:
+
+- Support a many-to-many relationship between LP documents and source files.
+  - In `noweb`, a document may contain several root chunks which can each be sent to different
+    source files, and multiple LP documents can be fed into the `tangle` tool at once (they are
+    concatenated).
+- The more modern tools support multiple programming and markup languages.
+- `noweb` offers a pipelined architecture which makes it easy to insert processing stages to meet a
+  user's needs.
+- They generate indices and cross references within the human-readable output.
+
+## Using `Verso / Recto`
+
+`Verso / Recto` is driven by annotations within your source files, defining regions which can be
+referenced by other documents.
+
+### Annotating a file
+
+Annotations are quite simple. To mark a region of code for reference, simply add a pair of comments
+around the region with the symbols `@!` and `!@`, followed by a unique ID. The ID can be any string
+of non-whitespace characters, though it should be both unique within your project and valid in the
+source file you're annotating.
+
+### Referencing annotations
+
+In order to reference an annotation in another file, add a line containing the symbol `@@` followed
+by the ID of the annotation. When the file is woven using the `recto` command (see the next
+section), the line will be replaced with the contents of the annotation. You can add any markup you
+like around the line to provide formatting.
+
+### Weaving a document for human consumption
+
+The `verso` command will read all of the annotations from the files specified on the command line,
+extract their annotations, and output the result to stdout. In turn the `recto` command, if the
+`--annotations-from` option is not used, will read its annotations from stdin. This makes the two
+programs easy to use together via pipes:
+
+`verso main.rs lib.rs | recto --dir build chap1.tex chap2.tex home.md`
+
+_(Conveniently, the `verso` program goes on the left of the pipe while `recto` goes on the right.)_
+
+## The Name
+
+> Recto and verso are respectively, the text written or printed on the "right" or "front" side and
+> on the "back" side of a leaf of paper in a bound item such as a codex, book, broadsheet, or
+> pamphlet.
+
+_(Wikipedia - Recto and Verso)[https://en.wikipedia.org/wiki/Recto_and_verso]_
+
+## Future Work
+
+- Add support for allow overlapping annotations.
+- Add support for custom formatting of annotation properties within the woven output.
---
+++ src/lib.rs
@@ -0,0 +1,233 @@
+use std::error::Error;
+use std::fmt;
+use std::fs;
+
+// Blocks are opened by using the form @!<id>. For example: @!202001171309.
+// The ID can be any set of characters terminated by whitespace.
+const BLOCK_OPEN_SYMBOL: &'static str = "@!";
+const BLOCK_CLOSE_SYMBOL: &'static str = "!@";
+
+#[derive(Debug,PartialEq)]
+pub struct Config {
+    pub filenames: Vec<String>,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+
+        let filenames = args[1..].iter().cloned().collect();
+
+        Ok(Config { filenames })
+    }
+}
+
+pub fn run(cfg: Config) -> Result<(), Box<dyn Error>>{
+    let annotations: Vec<(String, Vec<Fragment>)> = vec![];
+    for filename in cfg.filenames {
+        let contents = fs::read_to_string(filename)?;
+        let fragments = extract_fragments(contents)?;
+        annotations.push((filename, fragments));
+    }
+
+    Ok(())
+}
+
+#[derive(Debug, PartialEq, Clone)]
+struct Fragment {
+    body: String,
+    id: String,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+enum ParseError {
+    DoubleOpen{line: usize, col: usize},
+    CloseBeforeOpen{line: usize, col: usize},
+    MissingId{line: usize, col: usize},
+}
+
+impl Error for ParseError {}
+
+impl fmt::Display for ParseError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, match self {
+            DoubleOpen => ,
+            CloseBeforeOpen => ,
+            MissingId => ,
+        });
+    }
+}
+
+fn extract_fragments(contents: String) -> Result<Vec<Fragment>, ParseError> {
+    let mut fragments: Vec<Fragment> = vec![];
+
+    let mut fragment: Option<Fragment> = None;
+
+    for (line, content) in contents.split("\n").enumerate().map(|(l, c)| (l+1, c)) {
+        match &fragment {
+            None => {
+                if let Some(col) = content.find(BLOCK_CLOSE_SYMBOL) {
+                    return Err(ParseError::CloseBeforeOpen{ line, col });
+                }
+
+                if let Some(col) = content.find(BLOCK_OPEN_SYMBOL) {
+                    // If the line contains a start marker, begin a fragment file.
+                    if let Some(id) = extract_id(content, col + BLOCK_OPEN_SYMBOL.len()) {
+                        fragment = Some(Fragment {
+                            body: String::new(),
+                            id
+                        });
+                    } else {
+                        return Err(ParseError::MissingId{ line, col });
+                    }
+                }
+            },
+
+            Some(f) => {
+                // If the line contains an end marker, end the fragment if one exists.
+                if let Some(_) = content.find(BLOCK_CLOSE_SYMBOL) {
+                    fragments.push(f.to_owned());
+                    continue;
+                }
+
+                if let Some(col) = content.find(BLOCK_OPEN_SYMBOL) {
+                    return Err(ParseError::DoubleOpen{ line, col });
+                }
+
+                // If there no markers, append the line to the existing fragment.
+                fragment = fragment.map(|x| Fragment {
+                    body: if x.body.is_empty() {
+                        content.to_string()
+                    } else {
+                        x.body + "\n" + content
+                    },
+                    id: x.id
+                });
+            },
+        }
+    }
+
+    Ok(fragments)
+}
+
+fn extract_id(content: &str, col: usize) -> Option<String> {
+    let it = content.chars().skip(col);
+    let id: String = it.take_while(|c| !c.is_whitespace()).collect();
+    if id.is_empty() {
+        None
+    } else {
+        Some(id)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_extract_id_good() {
+        let id = extract_id(&String::from("foobarbaz"), 0);
+        assert!(id.expect("Expected successful ID extraction") == String::from("foobarbaz"));
+    }
+
+    #[test]
+    fn test_extract_id_missing() {
+        let id = extract_id(&String::from(""), 0);
+        assert!(id == None, "Expected None, got {:?}", id);
+    }
+
+    #[test]
+    fn test_extract_id_nonalphanumeric() {
+        let id = extract_id(&String::from("foo-bar-baz"), 0);
+        assert!(id.expect("Expected successful ID extraction") == String::from("foo-bar-baz"));
+    }
+
+    #[test]
+    fn test_extract_id_whitespace() {
+        let id = extract_id(&String::from("foo-bar-baz quuz"), 0);
+        assert!(id.expect("Expected successful ID extraction") == String::from("foo-bar-baz"));
+    }
+
+    #[test]
+    fn test_extract_id_offset() {
+        let id = extract_id(&String::from("foo-bar-baz quuz"), 4);
+        assert!(id.expect("Expected successful ID extraction") == String::from("bar-baz"));
+    }
+
+    #[test]
+    fn test_extract_fragments() {
+        let fragments: Result<Vec<Fragment>, ParseError> = extract_fragments(String::from("# This is an example
+import sys
+
+# @!foo-bar-baz The fragment starts and its ID is defined on this line; it is foo-bar-baz.
+def main():
+    do_stuff()
+    make_awesome()
+    # !@ This line ends the fragment.
+    sys.exit(1) # oops"));
+
+        let fragments = fragments.expect("Expected no parse errors");
+        assert!(fragments.len() == 1, "Expected one fragment, found {}", fragments.len());
+        assert!(fragments[0].body == String::from("def main():
+    do_stuff()
+    make_awesome()"), "Unexpected code fragment {:?}", fragments[0].body);
+        assert!(fragments[0].id == String::from("foo-bar-baz"), "Unexpected ID {:?}", fragments[0].id);
+    }
+
+    #[test]
+    fn test_extract_fragments_close_before_open() {
+        let fragments: Result<Vec<Fragment>, ParseError> = extract_fragments(String::from(
+            "# This is an example.
+
+# !@ This is an error on line 3.
+012345 The error is at column 2.
+# @! This begins the fragment.
+# !@ This line ends the fragment."));
+
+        let fragments = fragments.expect_err("Expected a parsing error");
+        match fragments {
+            ParseError::CloseBeforeOpen{line, col} => {
+                assert!(line == 3, "Expected error on line 3, found line {:?}", line);
+                assert!(col == 2, "Expected error on col 2, found col {:?}", col);
+            }
+            _ => panic!("Expected ParseError::CloseBeforeOpen, got {:?}", fragments)
+        }
+    }
+
+    #[test]
+    fn test_extract_fragments_double_open() {
+        let fragments: Result<Vec<Fragment>, ParseError> = extract_fragments(String::from(
+            "# This is an example.
+# @!foo-bar-baz This begins the fragment.
+# @! This is an error on line 3.
+012345 The error is at column 2.
+# !@ This line ends the fragment."));
+
+        let fragments = fragments.expect_err("Expected a parsing error");
+        match fragments {
+            ParseError::DoubleOpen{line, col} => {
+                assert!(line == 3, "Expected error on line 3, found line {:?}", line);
+                assert!(col == 2, "Expected error on col 2, found col {:?}", col);
+            }
+            _ => panic!("Expected ParseError::DoubleOpen, got {:?}", fragments)
+        }
+    }
+
+    #[test]
+    fn test_extract_fragments_missing_id() {
+        let fragments: Result<Vec<Fragment>, ParseError> = extract_fragments(String::from(
+            "# This is an example.
+
+# @! This is an error on line 3: No ID.
+012345 The error is at column 2.
+# !@ This line ends the fragment."));
+
+        let fragments = fragments.expect_err("Expected a parsing error");
+        match fragments {
+            ParseError::MissingId{line, col} => {
+                assert!(line == 3, "Expected error on line 3, found line {:?}", line);
+                assert!(col == 2, "Expected error on col 2, found col {:?}", col);
+            }
+            _ => panic!("Expected ParseError::MissingId, got {:?}", fragments)
+        }
+    }
+}
