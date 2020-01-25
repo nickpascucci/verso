0b2e94fb9b39422d63bf93d95d97f2c2202cecb6
Nick Pascucci <npascut1@gmail.com>
2020-01-25 19:46:15 +0000

    Add example using Verso to build a presentation with code snippets.
    Squashed commit of the following:
    
    commit 95445b2f59c75addbb0557e51fa4b4d466e68169
    Author: Nick Pascucci <npascut1@gmail.com>
    Date:   Thu Jan 23 09:09:50 2020 +0000
    
        Fix some minor bugs and expand the presentation.
    
    commit 94eea45866e981cc571827164635aba77266c3a5
    Author: Nick Pascucci <npascut1@gmail.com>
    Date:   Tue Jan 21 09:57:51 2020 +0000
    
        Minor tweaks
    
    commit de7ec15d6ce331df67c67baefb677f4ba839267f
    Author: Nick Pascucci <npascut1@gmail.com>
    Date:   Mon Jan 20 22:58:55 2020 +0000
    
        Remove unneeded annotation comments.
    
    commit 6f572fcae1d887273980b9bb1f9be1b31edce88b
    Author: Nick Pascucci <nick.pascucci@spire.com>
    Date:   Tue Jan 21 09:47:09 2020 +0000
    
        Replace home-grown macro with standard one.
    
    commit 6547d8579b1019fc820959170376360e0e5d0458
    Author: Nick Pascucci <npascut1@gmail.com>
    Date:   Mon Jan 20 22:56:23 2020 +0000
    
        Add a slimmed-down, basic Reveal presentation.
    
    commit df12a06cba7532629d99a75e8e82cb7ced8beaf4
    Author: Nick Pascucci <npascut1@gmail.com>
    Date:   Mon Jan 20 22:45:22 2020 +0000
    
        Use a macro to work around compile-time concatenation.
    
    commit 647dfde0498581cdf3cb222c4d04c84ba331c509
    Author: Nick Pascucci <npascut1@gmail.com>
    Date:   Mon Jan 20 21:49:01 2020 +0000
    
        Split tests out into a separate file.
    
    commit b2aef753609e0cb87a8544188aadac7f4563072a
    Author: Nick Pascucci <npascut1@gmail.com>
    Date:   Mon Jan 20 21:43:43 2020 +0000
    
        Split up the annotation constants so they don't match.

--- src/lib.rs
+++ src/lib.rs
@@ -4,10 +4,8 @@ use std::collections::HashMap;
 use std::error::Error;
 use std::fmt;
 
-// Blocks are opened by using the form @<id. For example: @!202001171309.
-// The ID can be any set of alphanumeric characters.
-const BLOCK_OPEN_SYMBOL: &'static str = "@<";
-const BLOCK_CLOSE_SYMBOL: &'static str = ">@";
+const BLOCK_OPEN_SYMBOL: &'static str = concat!("@", "<");
+const BLOCK_CLOSE_SYMBOL: &'static str = concat!(">", "@");
 const INSERTION_SYMBOL: &'static str = "@@";
 
 #[derive(Debug, PartialEq, Clone, Serialize, Deserialize)]
@@ -26,6 +24,7 @@ pub enum ParseError {
     MissingId,
 }
 
+// @<errors
 #[derive(Debug, PartialEq, Clone)]
 pub enum WeaveError {
     MissingFragment(String),
@@ -39,6 +38,7 @@ pub struct FileError<T: fmt::Debug> {
     line: usize,
     col: usize,
 }
+// >@errors
 
 impl<T: fmt::Debug> Error for FileError<T> {}
 
@@ -98,6 +98,7 @@ pub fn extract_fragments(
                 // If the line contains an end marker, end the fragment if one exists.
                 if let Some(_) = content.find(BLOCK_CLOSE_SYMBOL) {
                     fragments.push(f.to_owned());
+                    fragment = None;
                     continue;
                 }
 
@@ -126,6 +127,7 @@ pub fn extract_fragments(
     Ok(fragments)
 }
 
+// @<extractid
 fn extract_id(content: &str, col: usize) -> Option<String> {
     let it = content.chars().skip(col);
     let id: String = it.take_while(|c| c.is_alphanumeric()).collect();
@@ -135,6 +137,7 @@ fn extract_id(content: &str, col: usize) -> Option<String> {
         Some(id)
     }
 }
+// >@extractid
 
 pub fn weave(
     filename: &str,
@@ -179,9 +182,19 @@ pub fn weave(
     return Ok(substrings.join("\n"));
 }
 
+// @<tests
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_extract_id_missing() {
+        let id = extract_id(&String::from(""), 0);
+        assert_eq!(id, None, "Expected None, got {:?}", id);
+    }
+    // ... snip ...
+    // >@tests
+
 
     #[test]
     fn test_extract_id_good() {
@@ -190,12 +203,6 @@ mod tests {
             id.expect("Expected successful ID extraction"),
             String::from("foobarbaz")
         );
-    }
-
-    #[test]
-    fn test_extract_id_missing() {
-        let id = extract_id(&String::from(""), 0);
-        assert_eq!(id, None, "Expected None, got {:?}", id);
     }
 
     #[test]
@@ -223,166 +230,5 @@ mod tests {
             id.expect("Expected successful ID extraction"),
             String::from("bar")
         );
-    }
-
-    #[test]
-    fn test_extract_fragments() {
-        let fragments: Result<Vec<Fragment>, FileError<ParseError>> = extract_fragments(
-            "# This is an example
-import sys
-
-# @<foobarbaz The fragment starts and its ID is defined on this line; it is foobarbaz.
-def main():
-    do_stuff()
-    make_awesome()
-    # >@ This line ends the fragment.
-    sys.exit(1) # oops",
-            "test.py",
-        );
-
-        let fragments = fragments.expect("Expected no parse errors");
-        assert!(
-            fragments.len() == 1,
-            "Expected one fragment, found {}",
-            fragments.len()
-        );
-        assert!(
-            fragments[0].body
-                == String::from(
-                    "def main():
-    do_stuff()
-    make_awesome()"
-                ),
-            "Unexpected code fragment {:?}",
-            fragments[0].body
-        );
-        assert!(
-            fragments[0].id == String::from("foobarbaz"),
-            "Unexpected ID {:?}",
-            fragments[0].id
-        );
-    }
-
-    #[test]
-    fn test_extract_fragments_close_before_open() {
-        let fragments: Result<Vec<Fragment>, FileError<ParseError>> = extract_fragments(
-            "# This is an example.
-
-# >@ This is an error on line 3.
-012345 The error is at column 2.
-# @< This begins the fragment.
-# >@ This line ends the fragment.",
-            "test.py",
-        );
-
-        let fragments = fragments.expect_err("Expected a parsing error");
-        match fragments {
-            FileError {
-                err_type: ParseError::CloseBeforeOpen,
-                line,
-                col,
-                ..
-            } => {
-                assert_eq!(line, 3, "Expected error on line 3, found line {:?}", line);
-                assert_eq!(col, 2, "Expected error on col 2, found col {:?}", col);
-            }
-            _ => panic!("Expected ParseError::CloseBeforeOpen, got {:?}", fragments),
-        }
-    }
-
-    #[test]
-    fn test_extract_fragments_double_open() {
-        let fragments: Result<Vec<Fragment>, FileError<ParseError>> = extract_fragments(
-            "# This is an example.
-# @<foobarbaz This begins the fragment.
-# @< This is an error on line 3.
-012345 The error is at column 2.
-# >@ This line ends the fragment.",
-            "test.py",
-        );
-
-        let fragments = fragments.expect_err("Expected a parsing error");
-        match fragments {
-            FileError {
-                err_type: ParseError::DoubleOpen,
-                line,
-                col,
-                ..
-            } => {
-                assert_eq!(line, 3, "Expected error on line 3, found line {:?}", line);
-                assert_eq!(col, 2, "Expected error on col 2, found col {:?}", col);
-            }
-            _ => panic!("Expected ParseError::DoubleOpen, got {:?}", fragments),
-        }
-    }
-
-    #[test]
-    fn test_extract_fragments_missing_id() {
-        let fragments: Result<Vec<Fragment>, FileError<ParseError>> = extract_fragments(
-            "# This is an example.
-
-# @< This is an error on line 3: No ID.
-012345 The error is at column 2.
-# >@ This line ends the fragment.",
-            "test.py",
-        );
-
-        let fragments = fragments.expect_err("Expected a parsing error");
-        match fragments {
-            FileError {
-                err_type: ParseError::MissingId,
-                line,
-                col,
-                ..
-            } => {
-                assert_eq!(line, 3, "Expected error on line 3, found line {:?}", line);
-                assert_eq!(col, 2, "Expected error on col 2, found col {:?}", col);
-            }
-            _ => panic!("Expected ParseError::MissingId, got {:?}", fragments),
-        }
-    }
-
-    #[test]
-    fn test_weave_good() {
-        let text = "This is the first line!
-
-@@1
-
-Another line.";
-
-        let frag = Fragment {
-            id: String::from("1"),
-            body: String::from("{Example Code}"),
-            file: String::from("example.code"),
-            line: 1,
-            col: 0,
-        };
-
-        let mut annotations = HashMap::new();
-        annotations.insert(frag.id.to_owned(), frag.to_owned());
-        let result = weave("test", &text, &annotations).expect("Expected weave to return Ok");
-
-        assert_eq!(
-            result,
-            String::from(
-                "This is the first line!
-
-{Example Code}
-
-Another line."
-            )
-        );
-    }
-
-    #[test]
-    fn test_weave_missing_fragment() {
-        let text = "This is the first line!
-
-@@1
-
-Another line.";
-
-        let annotations = HashMap::new();
-        weave("test", &text, &annotations).expect_err("Expected weave to return an error");
     }
 }
