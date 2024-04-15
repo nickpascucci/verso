3e041fa53381325b8213dd176de3709c90527b49
Kevin Glasson <kevinglasson@gmail.com>
2024-04-15 21:19:01 +0800

    feat: add nested to check-examples
    Signed-off-by: Kevin Glasson <kevinglasson@gmail.com>

--- examples/check-examples.sh
+++ examples/check-examples.sh
@@ -4,7 +4,7 @@ set -e
 
 DIFF=$(which colordiff || echo "diff")
 
-SOURCE_FILES="example.py test/example-2.py"
+SOURCE_FILES="example.py test/example-2.py test/nested.rs"
 PROSE_FILES="empty.md example.md test/example-2.md test/level-2/l2.md"
 OUTPUT_DIRECTORY=out
 
